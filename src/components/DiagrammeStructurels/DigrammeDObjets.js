e14e31e93aad3c523fa5984b5ea9616cc06757bc
Theo Pierson <thheopierson@gmail.com>
2023-09-27 10:44:45 +0200

    feat: write docs for object diagram and add image to the component

--- src/components/DiagrammeStructurels/DigrammeDObjets.js
+++ src/components/DiagrammeStructurels/DigrammeDObjets.js
@@ -9,6 +9,44 @@ const DiagrammeDObjets = () => {
         artefacts sur les nœuds. Les diagrammes de déploiement sont considérés comme pertinents dans le cas où votre
         solution logicielle est distribuée sur différentes machines, chacune ayant une configuration distincte.
       </p>
+      <p>
+        Supposons que nous voulons créer un diagramme d'objets pour représenter un système de bibliothèque.
+        <li>
+          - Objets : Dans un diagramme d'objets, les objets sont comme les entités concrètes ou les instances des
+          classes que nous voulons représenter. Par exemple, nous pourrions avoir un objet "Livre1" représentant un
+          livre spécifique dans la bibliothèque, un objet "Bibliothèque" représentant la bibliothèque elle-même, et un
+          objet "Utilisateur1" représentant un utilisateur qui emprunte un livre. Chaque objet est représenté par un
+          rectangle avec son nom.
+        </li>
+        <li>
+          - Attributs : Les attributs ne sont pas aussi importants dans un diagramme d'objets que dans un diagramme de
+          classes, car nous nous concentrons sur les instances spécifiques plutôt que sur les types de classe.
+          Cependant, si nécessaire, vous pouvez inclure des attributs spécifiques à chaque objet, tels que le titre d'un
+          livre ou le nom d'un utilisateur.
+        </li>
+        <li>
+          - Relations : Les relations dans un diagramme d'objets montrent comment les objets interagissent les uns avec
+          les autres à un moment donné. Par exemple, un objet "Utilisateur1" peut avoir une relation avec l'objet
+          "Livre1" pour indiquer que cet utilisateur a emprunté ce livre. Les relations sont généralement représentées
+          par des lignes pointillées ou des flèches qui connectent les objets.
+        </li>
+        <li>
+          - Valeurs : Les valeurs dans un diagramme d'objets représentent les données ou les valeurs spécifiques
+          associées à chaque objet. Par exemple, l'objet "Livre1" peut avoir une valeur pour son titre, son auteur et
+          son numéro d'inventaire.
+        </li>
+        <li>
+          - Messages : Dans un diagramme d'objets, vous pouvez également montrer les messages ou les interactions
+          spécifiques entre les objets. Par exemple, vous pourriez montrer un message "emprunter()" envoyé de l'objet
+          "Utilisateur1" à l'objet "Livre1" pour indiquer que l'utilisateur emprunte le livre.{" "}
+        </li>
+        En résumé, un diagramme d'objets UML est utilisé pour représenter les objets concrets et leurs interactions dans
+        un système à un moment donné. C'est comme une instantané d'un système en action. Vous montrez les objets
+        spécifiques, leurs relations, leurs valeurs, et parfois les messages qu'ils échangent. Cela aide à visualiser
+        comment les objets interagissent dans un scénario particulier, ce qui peut être utile pour comprendre le
+        comportement dynamique d'un système.
+      </p>
+      <img src="Object_Diagram.webp" alt="" />
     </div>
   );
 };
