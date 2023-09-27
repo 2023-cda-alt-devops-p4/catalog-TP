9992c53fde63da68e7286a89cf85166b1942f8f8
Theo Pierson <thheopierson@gmail.com>
2023-09-27 16:10:08 +0200

    feat:write down docs for diagramme de profil and add image to the file

--- src/components/DiagrammeStructurels/DiagrammeDeProfil.js
+++ src/components/DiagrammeStructurels/DiagrammeDeProfil.js
@@ -9,6 +9,41 @@ const DiagrammeDeProfil = () => {
         servent à étendre l’UML, qui est basé sur davantage de stéréotypes ajoutés et de valeurs balisées qui sont
         appliquées aux éléments, aux composants et aux connecteurs.{" "}
       </p>
+      <p>
+        Supposons que nous voulions créer un diagramme de profil pour étendre UML et définir des concepts spécifiques à
+        un domaine, comme la modélisation des transports publics.
+        <li>
+          Stéréotypes : Les stéréotypes sont des extensions personnalisées que vous ajoutez aux éléments UML standard
+          pour leur donner des significations spécifiques à votre domaine. Par exemple, nous pourrions définir un
+          stéréotype "Station" pour les éléments de modèle UML qui représentent des stations de transport.
+        </li>
+        <li>
+          Attributs de modèle : Les attributs de modèle permettent de spécifier des informations supplémentaires pour
+          les éléments UML étendus. Par exemple, pour le stéréotype "Station", nous pourrions ajouter des attributs
+          comme "Nom de la Station" et "Localisation Géographique".
+        </li>
+        <li>
+          Stéréotypes de relation : En plus de stéréotyper des éléments individuels, vous pouvez stéréotyper des
+          relations entre les éléments. Par exemple, vous pourriez définir un stéréotype "Ligne" pour indiquer les
+          relations entre les stations qui sont desservies par une ligne de transport spécifique.
+        </li>
+        <li>
+          Icones et représentations personnalisées : Vous pouvez également définir des icônes ou des représentations
+          personnalisées pour les éléments stéréotypés afin de les différencier visuellement des éléments UML standard.
+          Par exemple, vous pourriez utiliser une icône de station de métro pour représenter une station de transport
+          dans le diagramme.
+        </li>
+        <li>
+          Utilisation du profil : Une fois que vous avez défini votre profil, vous pouvez l'appliquer à vos diagrammes
+          UML pour utiliser les stéréotypes, les attributs de modèle et les relations personnalisées dans votre modèle.
+        </li>
+        En résumé, un diagramme de profil UML est utilisé pour personnaliser le langage UML afin de répondre aux besoins
+        spécifiques d'un domaine ou d'une application particulière. Il vous permet d'ajouter des concepts, des attributs
+        et des relations personnalisés, ainsi que des représentations visuelles spécifiques pour mieux représenter et
+        documenter votre modèle dans le contexte de votre domaine d'application. Cela facilite la communication et la
+        modélisation dans des domaines spécialisés tels que les transports publics, la santé, l'aérospatiale, etc.
+      </p>
+      <img src="UML_use_case_example1-750x731.webp" alt="" />
     </div>
   );
 };
