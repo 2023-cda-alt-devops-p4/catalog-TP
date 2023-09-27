37cec882fdc258864623f620a18a9ff86e269ebe
Theo Pierson <thheopierson@gmail.com>
2023-09-27 14:18:59 +0200

    feat: write diagramme de cas d'utilisation docs and add image to the file

--- src/components/DiagrammeStructurels/DiagrammeDActivite.js
+++ src/components/DiagrammeStructurels/DiagrammeDActivite.js
@@ -9,6 +9,50 @@ const DiagrammeDActivite = () => {
         certain système qui est identique à un organigramme ou un diagramme de flux de données. Le diagramme d’activité
         a généralement un début ou «l’état initial» et une fin qui est l’état final ».
       </p>
+      <p>
+        Supposons que nous voulions créer un diagramme d'activité pour représenter le processus de réservation d'une
+        chambre d'hôtel en ligne.
+        <li>
+          - Actions : Les actions sont comme les étapes ou les actions spécifiques dans un processus. Par exemple, vous
+          pourriez avoir des actions telles que "Sélectionner la date d'arrivée", "Sélectionner la date de départ",
+          "Choisir le type de chambre", "Vérifier la disponibilité", "Saisir les informations du client", "Effectuer le
+          paiement", "Confirmer la réservation", etc. Chaque action est représentée par une forme ovale avec une
+          étiquette.
+        </li>
+        <li>
+          - Flux de contrôle : Les flèches indiquent la séquence dans laquelle les actions sont exécutées. Vous utilisez
+          des flèches pour relier les actions dans l'ordre approprié, montrant comment le contrôle passe d'une action à
+          l'autre.
+        </li>
+        <li>
+          - Décisions (branchement) : Les losanges (formes en diamant) représentent des points de décision dans le
+          processus. Par exemple, vous pourriez avoir une décision pour vérifier si la chambre souhaitée est disponible.
+          Les flèches sortantes d'une décision indiquent les différentes voies possibles en fonction des conditions,
+          comme "Oui" ou "Non".
+        </li>
+        <li>
+          - Fusion (jonction) : Les barres verticales (formes en barre) représentent les points où plusieurs chemins se
+          rejoignent. Par exemple, après avoir vérifié la disponibilité, vous pourriez avoir une jonction où le
+          processus reprend pour effectuer le paiement, quelle que soit la disponibilité de la chambre.
+        </li>
+        <li>
+          - États initiaux et finaux : Un cercle rempli représente l'état initial, c'est là où le processus commence. Un
+          cercle avec un anneau indique l'état final, c'est là où le processus se termine.
+        </li>
+        <li>
+          - Activités spéciales : Vous pouvez également utiliser des symboles spéciaux pour représenter des activités
+          particulières, comme des appels à des sous-processus (activité appelée), des boucles (activité boucle), ou des
+          synchronisations (activité de synchronisation).
+        </li>
+        <li>
+          - Nageurs (swimlanes) : Si plusieurs acteurs ou départements sont impliqués dans le processus, vous pouvez
+          diviser le diagramme en "nageurs" pour indiquer qui est responsable de chaque action.{" "}
+        </li>
+        En résumé, un diagramme d'activité UML est utilisé pour modéliser les étapes d'un processus ou d'un flux de
+        travail. Il représente les actions, les décisions, les points de jonction et les transitions entre ces éléments
+        pour illustrer comment un processus se déroule. C'est utile pour comprendre, concevoir et documenter des
+        procédures ou des systèmes qui impliquent une séquence d'actions.
+      </p>
       <img src="activity-diagram-for-login-UML-650x797.webp" alt="" />
     </div>
   );
--- src/components/DiagrammeStructurels/DiagrammeDeCasDUtilisation.js
+++ src/components/DiagrammeStructurels/DiagrammeDeCasDUtilisation.js
@@ -11,6 +11,46 @@ const DiagrammeDeCasDUtilisation = () => {
         rôles spécifiques trouvés dans le système sont les «acteurs». Et le «système» est celui qui est en cours de
         développement.
       </p>
+      <p>
+        Supposons que nous voulions créer un diagramme de cas d'utilisation pour représenter un système de réservation
+        en ligne pour un cinéma.
+        <li>
+          - Acteurs : Les acteurs sont comme les utilisateurs ou les entités externes qui interagissent avec le système.
+          Dans ce cas, nous pourrions avoir des acteurs tels que "Client" (les personnes qui réservent des billets) et
+          "Gestionnaire de Cinéma" (les employés qui gèrent les informations sur les films et les salles). Les acteurs
+          sont généralement représentés par des silhouettes d'humains ou des icônes.
+        </li>
+        <li>
+          - Cas d'utilisation : Les cas d'utilisation sont comme les fonctionnalités ou les actions que les acteurs
+          peuvent effectuer dans le système. Par exemple, nous pourrions avoir des cas d'utilisation tels que "Réserver
+          un billet", "Consulter les horaires des films", "Annuler une réservation", "Ajouter un nouveau film au
+          catalogue", etc. Chaque cas d'utilisation est représenté par une ellipse avec un nom.
+        </li>
+        <li>
+          - Relations acteur-cas d'utilisation : Les lignes solides ou les flèches connectent les acteurs aux cas
+          d'utilisation pour montrer quel acteur peut effectuer quelles actions. Par exemple, le cas d'utilisation
+          "Réserver un billet" serait relié à l'acteur "Client" pour montrer que les clients peuvent effectuer cette
+          action.
+        </li>
+        <li>
+          - Inclusion et extension (séquences) : Les diagrammes de cas d'utilisation peuvent également montrer comment
+          les cas d'utilisation sont liés les uns aux autres. L'inclusion indique qu'un cas d'utilisation peut inclure
+          un autre cas d'utilisation. Par exemple, le cas d'utilisation "Réserver un billet" pourrait inclure le cas
+          d'utilisation "Sélectionner des sièges". L'extension indique que certaines actions peuvent être étendues par
+          d'autres actions en fonction de conditions. Par exemple, l'action "Annuler une réservation" peut être étendue
+          par "Rembourser le paiement" si le client le demande.
+        </li>
+        <li>
+          - Acteur étendu : Parfois, un acteur peut être étendu pour inclure plusieurs sous-acteurs. Par exemple, le
+          "Client" peut être étendu pour inclure des sous-acteurs tels que "Client enregistré" et "Client invité",
+          chacun ayant ses propres capacités.{" "}
+        </li>
+        En résumé, un diagramme de cas d'utilisation UML est utilisé pour modéliser les interactions entre les acteurs
+        et les fonctionnalités d'un système. Il montre qui fait quoi dans le système et comment les acteurs
+        interagissent avec les différentes fonctionnalités. Cela aide à comprendre les besoins des utilisateurs et à
+        concevoir le système en fonction de ces besoins.
+      </p>
+      <img src="UML_use_case_example-800x707.png" alt="" />
     </div>
   );
 };
