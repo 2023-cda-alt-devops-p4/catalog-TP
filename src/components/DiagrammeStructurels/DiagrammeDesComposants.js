2148619adddb95ace1bdd01c70aa28830d876328
Theo Pierson <thheopierson@gmail.com>
2023-09-27 09:56:33 +0200

    feat: write Diagramme Deployement docs and add image to the component

--- src/components/DiagrammeStructurels/DiagrammeDesComposants.js
+++ src/components/DiagrammeStructurels/DiagrammeDesComposants.js
@@ -3,13 +3,14 @@ import React from "react";
 const DiagrammeDesComposants = () => {
   return (
     <div>
-      <h3>Diagramme des composants</h3>
+      <h3>Diagramme des Composants</h3>
       <p>
         Le diagramme des composants illustre la manière dont les composants sont combinés pour créer des composants plus
         volumineux. Ces types de diagrammes sont utiles pour travailler sur des systèmes complexes comportant de
         nombreux composants. Les composants se relaient les uns aux autres grâce à l’utilisation d’interfaces, qui sont
         ensuite reliées à l’aide de connecteurs.
       </p>
+      <img src="New-Component-Diagram-for-Library-Management-System.png" alt="" />
     </div>
   );
 };
