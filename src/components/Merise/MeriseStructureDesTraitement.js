f50d6fa4c72c11042974885ea554c70bf325533a
Theo Pierson <thheopierson@gmail.com>
2023-10-01 09:45:25 +0200

    docs: change list style in html

--- src/components/Merise/MeriseStructureDesTraitement.js
+++ src/components/Merise/MeriseStructureDesTraitement.js
@@ -11,38 +11,38 @@ const MeriseStructureDesTraitement = () => {
         organisées et hiérarchisées à l'intérieur d'une unité de traitement. Voici une explication plus détaillée des
         éléments clés d'un Diagramme de Structure des Traitements (DST) :
         <li>
-          Processus : Le processus principal est représenté comme une boîte ou un rectangle et représente l'unité de
+          -Processus : Le processus principal est représenté comme une boîte ou un rectangle et représente l'unité de
           traitement globale que vous souhaitez modéliser. Ce processus est généralement le module logiciel, la fonction
           ou la tâche que vous décomposez en sous-processus. Chaque processus est identifié par un numéro ou un nom
           significatif.
         </li>
         <li>
-          Sous-Processus : Les sous-processus sont des unités de traitement plus petites et plus détaillées qui
+          - Sous-Processus : Les sous-processus sont des unités de traitement plus petites et plus détaillées qui
           composent le processus principal. Ils sont généralement représentés par des rectangles internes au processus
           principal. Les sous-processus représentent des tâches spécifiques ou des étapes dans le traitement global.
         </li>
         <li>
-          Flux de Contrôle : Les flèches ou les lignes qui relient les processus et les sous-processus indiquent l'ordre
-          dans lequel les tâches sont exécutées. Les flèches montrent la direction du flux de contrôle, c'est-à-dire
-          l'ordre dans lequel les sous-processus sont exécutés.
+          - Flux de Contrôle : Les flèches ou les lignes qui relient les processus et les sous-processus indiquent
+          l'ordre dans lequel les tâches sont exécutées. Les flèches montrent la direction du flux de contrôle,
+          c'est-à-dire l'ordre dans lequel les sous-processus sont exécutés.
         </li>
         <li>
-          Flux de Données : Les flux de données représentent les données qui sont transmises entre les processus et les
-          sous-processus. Ils indiquent comment les informations circulent à l'intérieur du système et sont généralement
-          représentés par des flèches avec des étiquettes pour identifier les données échangées.
+          - Flux de Données : Les flux de données représentent les données qui sont transmises entre les processus et
+          les sous-processus. Ils indiquent comment les informations circulent à l'intérieur du système et sont
+          généralement représentés par des flèches avec des étiquettes pour identifier les données échangées.
         </li>
         <li>
-          Entrées et Sorties : Chaque processus ou sous-processus peut avoir des entrées (données requises pour exécuter
-          la tâche) et des sorties (résultats produits par la tâche). Les entrées et les sorties sont généralement
-          indiquées à l'extérieur des rectangles représentant les processus.
+          - Entrées et Sorties : Chaque processus ou sous-processus peut avoir des entrées (données requises pour
+          exécuter la tâche) et des sorties (résultats produits par la tâche). Les entrées et les sorties sont
+          généralement indiquées à l'extérieur des rectangles représentant les processus.
         </li>
         <li>
-          Contrôles : Les contrôles, les règles de traitement, les conditions et les boucles peuvent être indiqués à
+          - Contrôles : Les contrôles, les règles de traitement, les conditions et les boucles peuvent être indiqués à
           l'intérieur des rectangles pour montrer comment les données sont traitées à chaque étape du processus. Les
           contrôles détaillent la logique et les décisions prises à chaque niveau.
         </li>
         <li>
-          Regroupements : Des regroupements ou des regroupements logiques de sous-processus peuvent être utilisés pour
+          - Regroupements : Des regroupements ou des regroupements logiques de sous-processus peuvent être utilisés pour
           organiser et simplifier la représentation du DST. Ils peuvent être utilisés pour montrer que plusieurs
           sous-processus sont liés d'une certaine manière.
         </li>
